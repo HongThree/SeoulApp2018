5f417a87e85ddb12c7a49a86548b45eb75dab0a8
deadlockk <style8914@naver.com>
2018-08-06 13:27:24 +0900

    update

--- app/src/main/java/com/example/kihunahn/seoulapp2018/CourseFragment.kt
+++ app/src/main/java/com/example/kihunahn/seoulapp2018/CourseFragment.kt
@@ -1,17 +1,22 @@
 package com.example.kihunahn.seoulapp2018
 
-import com.imangazaliev.circlemenu.CircleMenuButton
-
-import com.imangazaliev.circlemenu.CircleMenu
-import android.support.design.widget.Snackbar
 import android.view.ViewGroup
-import android.support.v7.app.AppCompatDelegate
+
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
+
 import kotlinx.android.synthetic.main.fragment_course.*
+import com.nightonke.boommenu.BoomButtons.ButtonPlaceEnum
+import com.nightonke.boommenu.Piece.PiecePlaceEnum
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+import com.nightonke.boommenu.BoomButtons.HamButton
+import com.nightonke.boommenu.ButtonEnum
+import com.nightonke.boommenu.BoomMenuButton
 
 
 class CourseFragment : Fragment() {
@@ -20,33 +25,18 @@ class CourseFragment : Fragment() {
 
         val view = inflater?.inflate(R.layout.fragment_course, container, false)
 
-        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
+
+
+        assert(bmb != null)
+        bmb.buttonEnum = ButtonEnum.Ham
+        bmb.piecePlaceEnum = PiecePlaceEnum.HAM_6
+        bmb.buttonPlaceEnum = ButtonPlaceEnum.HAM_6
+
+        for (i in 0 until bmb.piecePlaceEnum.pieceNumber()) {
+            val builder = HamButton.Builder()
+            bmb.addBuilder(builder)
+        }
+
         return view
-
-        val snackbarContainer = snackbar_contaner
-        val circleMenu = circle_menu
-        circleMenu.setOnItemClickListener(CircleMenu.OnItemClickListener { menuButton -> Snackbar.make(snackbarContainer, menuButton.hintText, Snackbar.LENGTH_LONG).show() })
-
-        circleMenu.setEventListener(object : CircleMenu.EventListener {
-            override fun onMenuOpenAnimationStart() {
-                Log.d("CircleMenuStatus", "onMenuOpenAnimationStart")
-            }
-            override fun onMenuOpenAnimationEnd() {
-                Log.d("CircleMenuStatus", "onMenuOpenAnimationEnd")
-            }
-            override fun onMenuCloseAnimationStart() {
-                Log.d("CircleMenuStatus", "onMenuCloseAnimationStart")
-            }
-            override fun onMenuCloseAnimationEnd() {
-                Log.d("CircleMenuStatus", "onMenuCloseAnimationEnd")
-            }
-            override fun onButtonClickAnimationStart(menuButton: CircleMenuButton) {
-                Log.d("CircleMenuStatus", "onButtonClickAnimationStart")
-            }
-            override fun onButtonClickAnimationEnd(menuButton: CircleMenuButton) {
-                Log.d("CircleMenuStatus", "onButtonClickAnimationEnd")
-            }
-
-        })
     }
 }
