0113c6e0effa810f4417335488b34ac0eb3e560c
hanwjdgh <hanwjdgh@naver.com>
2018-08-17 16:02:37 +0900

    dialog and fragment

--- app/src/main/java/com/example/kihunahn/seoulapp2018/Fragment/Fragment2.kt
+++ app/src/main/java/com/example/kihunahn/seoulapp2018/Fragment/Fragment2.kt
@@ -1,13 +1,7 @@
 package com.example.kihunahn.seoulapp2018.Fragment
 
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
 import android.graphics.Color
-import android.location.LocationManager
 import android.os.Bundle
-import android.os.Handler
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -15,9 +9,6 @@ import android.view.ViewGroup
 import com.example.kihunahn.seoulapp2018.NMap.NMapFragment
 import com.example.kihunahn.seoulapp2018.NMap.NMapViewerResourceProvider
 import com.example.kihunahn.seoulapp2018.R
-import com.google.android.gms.common.api.GoogleApiClient
-import com.google.android.gms.common.api.ResultCallback
-import com.google.android.gms.location.*
 import com.nhn.android.maps.NMapCompassManager
 import com.nhn.android.maps.NMapController
 import com.nhn.android.maps.NMapLocationManager
@@ -41,12 +32,11 @@ class Fragment2 : NMapFragment(), NMapView.OnMapStateChangeListener, NMapPOIdata
     var mMyLocationOverlay: NMapMyLocationOverlay? = null
     var mMapCompassManager: NMapCompassManager? = null
 
-    var dlati: ArrayList<Double> = ArrayList()
-    var dloti: ArrayList<Double> = ArrayList()
+    var dlati:ArrayList<Double> = ArrayList()
+    var dloti:ArrayList<Double> = ArrayList()
 
-    var mGoogleApiClient: GoogleApiClient? = null
-    private val BROADCAST_ACTION = "android.location.PROVIDERS_CHANGED"
     override fun onMapCenterChangeFine(p0: NMapView?) {
+
     }
 
     override fun onAnimationStateChange(p0: NMapView?, p1: Int, p2: Int) {
@@ -77,11 +67,6 @@ class Fragment2 : NMapFragment(), NMapView.OnMapStateChangeListener, NMapPOIdata
 
     }
 
-    override fun onResume() {
-        super.onResume()
-        activity!!.registerReceiver(gpsLocationReceiver, IntentFilter(BROADCAST_ACTION))
-    }
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val v = inflater.inflate(R.layout.fragment1, container, false)
         mapView = (v.findViewById(R.id.mapView) as NMapView)
@@ -91,76 +76,16 @@ class Fragment2 : NMapFragment(), NMapView.OnMapStateChangeListener, NMapPOIdata
         mapView!!.isFocusable = true
         mapView!!.isFocusableInTouchMode = true
         mapView!!.requestFocus()
-
-        mGoogleApiClient = GoogleApiClient.Builder(activity!!)
-                .addApi(LocationServices.API)
-                .build()
-        mGoogleApiClient!!.connect()
-
-        showSettingDialog()
-
         return v
     }
 
-    private fun showSettingDialog() {
-        val locationRequest = LocationRequest.create()
-        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY //Setting priotity of Location request to high
-        locationRequest.interval = 1 * 1000
-        locationRequest.fastestInterval = 5 * 1000//5 sec Time interval for location update
-        val builder = LocationSettingsRequest.Builder()
-                .addLocationRequest(locationRequest)
-        builder.setAlwaysShow(true) //this is the key ingredient to show dialog always when GPS is off
-
-        val result = LocationServices.SettingsApi.checkLocationSettings(mGoogleApiClient, builder.build())
-        result.setResultCallback(ResultCallback<LocationSettingsResult> {result ->
-            var status = result.status
-            var state = result.locationSettingsStates
-            when(status.statusCode){
-                LocationSettingsStatusCodes.SUCCESS->{
-
-                }
-                LocationSettingsStatusCodes.RESOLUTION_REQUIRED->{
-
-                }
-                LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE->{
-
-                }
-            }
-
-        })
-    }
-
-    private val sendUpdatesToUI = Runnable { showSettingDialog() }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mGoogleApiClient = GoogleApiClient.Builder(activity!!)
-                .addApi(LocationServices.API)
-                .build()
-        mGoogleApiClient!!.connect()
-        showSettingDialog()
-    }
 
-    private val gpsLocationReceiver = object : BroadcastReceiver() {
-        override fun onReceive(context: Context?, intent: Intent?) {
-            Log.d("tlqkf",intent!!.action)
-            if (intent!!.action == BROADCAST_ACTION) {
-                var locationManager:LocationManager = context!!.getSystemService(Context.LOCATION_SERVICE) as LocationManager
-                Log.d("ASD","wlsdlq")
-                //Check if GPS is turned ON or OFF
-                if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-                    Log.e("About GPS", "GPS is Enabled in your device")
-                } else {
-                    Handler().postDelayed(sendUpdatesToUI,10)
-                    Log.e("About GPS", "GPS is Disabled in your device")
-                }
-            }
-        }
     }
 
     override fun onStart() {
         super.onStart()
-        showSettingDialog()
         mapView?.setBuiltInZoomControls(true, null)
         mapView?.setOnMapStateChangeListener(this)
         mapController = mapView?.mapController
@@ -213,33 +138,28 @@ class Fragment2 : NMapFragment(), NMapView.OnMapStateChangeListener, NMapPOIdata
         mMapLocationManager?.setOnLocationChangeListener(onMyLocationChangeListener)
         mMapLocationManager!!.enableMyLocation(true)
         mMyLocationOverlay = mapOverlayManager!!.createMyLocationOverlay(mMapLocationManager, mMapCompassManager)
+
     }
 
-    private fun drawline() {
-        if (dlati.size > 0) {
-            var len = dlati.size - 1
-            val currentPoint = NGeoPoint(dlati[len], dloti[len])
+    private fun drawline(){
+        if(dlati.size>0) {
+            var len = dlati.size-1
+            val currentPoint = NGeoPoint(dlati[0], dloti[0])
             mapController!!.mapCenter = currentPoint
             mapController!!.setZoomEnabled(true)
-            mapController!!.zoomLevel = 100
-
             val pathData = NMapPathData(len)
             pathData.initPathData()
-            for (i in 0..len) {
-                Log.d("ASD", dlati[i].toString() + " " + dloti[i].toString())
-                if (i == 0)
+            for(i in 0..len) {
+                if(i==0)
                     pathData.addPathPoint(dlati[i], dloti[i], NMapPathLineStyle.TYPE_SOLID)
                 else
                     pathData.addPathPoint(dlati[i], dloti[i], 0)
             }
             pathData.endPathData()
             val pathDataOverlay = mapOverlayManager!!.createPathDataOverlay(pathData)
-            pathDataOverlay.setLineColor(Color.RED, 1000)
         }
     }
-
     companion object {
         private val CLIENT_ID = "XrPrMsRtFpXGFaq_Az1I"// 애플리케이션 클라이언트 아이디 값
     }
 }
-
--- app/src/main/java/com/example/kihunahn/seoulapp2018/Fragment/MyCourseFragment.kt
+++ app/src/main/java/com/example/kihunahn/seoulapp2018/Fragment/MyCourseFragment.kt
@@ -1,17 +1,23 @@
 package com.example.kihunahn.seoulapp2018.Fragment
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.kihunahn.seoulapp2018.R
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.common.api.ResultCallback
+import com.google.android.gms.location.*
 import kotlinx.android.synthetic.main.fragment_mycourse.*
 
 
-
-
 class MyCourseFragment : Fragment() {
+    var mGoogleApiClient: GoogleApiClient? = null
+    private val REQUEST_CHECK_SETTINGS = 0x1
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
@@ -24,12 +30,11 @@ class MyCourseFragment : Fragment() {
 
 
         btn_start.setOnClickListener {
-            val fragment2 = MakeCourseFragment()
-            val fragmentManager = fragmentManager
-            val fragmentTransaction = fragmentManager!!.beginTransaction()
-            fragmentTransaction.replace(R.id.container, fragment2)
-            fragmentTransaction.addToBackStack(null)
-            fragmentTransaction.commit()
+            mGoogleApiClient = GoogleApiClient.Builder(activity!!)
+                    .addApi(LocationServices.API)
+                    .build()
+            mGoogleApiClient!!.connect()
+            showSettingDialog()
         }
 
         btn_archive.setOnClickListener {
@@ -41,6 +46,55 @@ class MyCourseFragment : Fragment() {
             fragmentTransaction.commit()
         }
 
+
+    }
+
+    private fun showSettingDialog() {
+        val locationRequest = LocationRequest.create()
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY //Setting priotity of Location request to high
+        locationRequest.interval = 30 * 1000
+        locationRequest.fastestInterval = 5 * 1000//5 sec Time interval for location update
+        val builder = LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest)
+        builder.setAlwaysShow(true) //this is the key ingredient to show dialog always when GPS is off
+
+        val result = LocationServices.SettingsApi.checkLocationSettings(mGoogleApiClient, builder.build())
+        result.setResultCallback(ResultCallback<LocationSettingsResult> { result ->
+            var status = result.status
+            var state = result.locationSettingsStates
+            when (status.statusCode) {
+                LocationSettingsStatusCodes.SUCCESS -> {
+
+                }
+                LocationSettingsStatusCodes.RESOLUTION_REQUIRED -> {
+                    this.startIntentSenderForResult(status.resolution.intentSender, REQUEST_CHECK_SETTINGS, null, 0, 0, 0, null)
+                }
+                LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE -> {
+
+                }
+            }
+
+        })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        Log.d("result1","QWEQWE")
+        when (requestCode) {
+            // Check for the integer request code originally supplied to startResolutionForResult().
+            REQUEST_CHECK_SETTINGS -> when (resultCode) {
+                Activity.RESULT_OK -> {
+                    val fragment2 = MakeCourseFragment()
+                    val fragmentManager = fragmentManager
+                    val fragmentTransaction = fragmentManager!!.beginTransaction()
+                    fragmentTransaction.replace(R.id.container, fragment2)
+                    fragmentTransaction.addToBackStack(null)
+                    fragmentTransaction.commit()
+                }
+                Activity.RESULT_CANCELED -> {
+                    Log.e("Settings", "Result Cancel")
+                }
+            }
+        }
     }
 
     override fun onResume() {
