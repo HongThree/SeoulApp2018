676aa12f7eb0b3c9787792e0a5f4695eb9f8f1a3
deadlockk <style8914@naver.com>
2018-08-31 14:07:39 +0900

    update home

---
+++ app/src/main/java/com/example/kihunahn/seoulapp2018/Adapter/PostsAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.kihunahn.seoulapp2018.Adapter
+
+import android.support.v4.app.FragmentActivity
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.kihunahn.seoulapp2018.R
+import com.example.kihunahn.seoulapp2018.model.Post
+import com.squareup.picasso.Picasso
+
+class PostsAdapter(val posts: ArrayList<Post>, val context: FragmentActivity?) : RecyclerView.Adapter<PostsAdapter.ViewHolder>() {
+
+    override fun getItemCount() = posts.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder{
+        val view : View = LayoutInflater.from(parent.context).inflate(R.layout.post_row, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.username.text = posts[position].username
+        holder.textfeed.text = posts[position].text
+        Picasso.get().load(posts[position].photo).into(holder.photo)
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val username: TextView = itemView.findViewById(R.id.username)
+        val textfeed: TextView = itemView.findViewById(R.id.textfeed)
+        val photo: ImageView = itemView.findViewById(R.id.photofeed)
+    }
+}
---
+++ app/src/main/java/com/example/kihunahn/seoulapp2018/model/Post.kt
@@ -0,0 +1,3 @@
+package com.example.kihunahn.seoulapp2018.model
+
+data class Post(val username: String, val text: String, val photo: String)
