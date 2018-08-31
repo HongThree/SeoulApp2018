347fa21ec2c820f62c13e47650e7bf91ab715a71
hanwjdgh <hanwjdgh@naver.com>
2018-08-31 17:00:26 +0900

    modify line color

--- app/src/main/java/com/example/kihunahn/seoulapp2018/Fragment/Fragment1.kt
+++ app/src/main/java/com/example/kihunahn/seoulapp2018/Fragment/Fragment1.kt
@@ -1,6 +1,5 @@
 package com.example.kihunahn.seoulapp2018.Fragment
 
-import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -27,18 +26,18 @@ class Fragment1 : NMapFragment(), NMapView.OnMapStateChangeListener, NMapPOIdata
     var mapController: NMapController? = null
     var mapViewerResourceProvider: NMapViewerResourceProvider? = null
     var mapOverlayManager: NMapOverlayManager? = null
-    var lati:ArrayList<String> = ArrayList()
-    var loti:ArrayList<String> = ArrayList()
-    var lati1:ArrayList<String> = ArrayList()
-    var loti1:ArrayList<String> = ArrayList()
+    var lati: ArrayList<String> = ArrayList()
+    var loti: ArrayList<String> = ArrayList()
+    var lati1: ArrayList<String> = ArrayList()
+    var loti1: ArrayList<String> = ArrayList()
 
-    var dlati:ArrayList<Double> = ArrayList()
-    var dloti:ArrayList<Double> = ArrayList()
-    var dlati1:ArrayList<Double> = ArrayList()
-    var dloti1:ArrayList<Double> = ArrayList()
-    var cnum:Int = 0
+    var dlati: ArrayList<Double> = ArrayList()
+    var dloti: ArrayList<Double> = ArrayList()
+    var dlati1: ArrayList<Double> = ArrayList()
+    var dloti1: ArrayList<Double> = ArrayList()
+    var cnum: Int = 0
 
-    var odds = intArrayOf(0,16737280,Color.MAGENTA,Color.BLUE,Color.GREEN,13107455,Color.RED,16750080,65535)
+    var odds = intArrayOf(15423769, 15423769, 15557287, 880568, 495930, 9570179, 12782883, 10842661, 1317215)
     override fun onMapCenterChangeFine(p0: NMapView?) {
 
     }
@@ -76,9 +75,9 @@ class Fragment1 : NMapFragment(), NMapView.OnMapStateChangeListener, NMapPOIdata
         mapView = (v.findViewById(R.id.mapView) as NMapView)
         mapView!!.setClientId(CLIENT_ID)
         mapView!!.isClickable = true
-        mapView!!.isEnabled=true
-        mapView!!.isFocusable=true
-        mapView!!.isFocusableInTouchMode=true
+        mapView!!.isEnabled = true
+        mapView!!.isFocusable = true
+        mapView!!.isFocusableInTouchMode = true
         mapView!!.requestFocus()
         return v
     }
@@ -90,21 +89,20 @@ class Fragment1 : NMapFragment(), NMapView.OnMapStateChangeListener, NMapPOIdata
         lati1 = arguments?.getStringArrayList("la1")!!
         loti1 = arguments?.getStringArrayList("lo1")!!
         cnum = arguments?.getInt("num")!!
-        Log.d("QWE",cnum.toString())
-        if(cnum==1){
-            var len = lati.size-1
+        Log.d("QWE", cnum.toString())
+        if (cnum == 1) {
+            var len = lati.size - 1
             for (i in 0..len) {
                 dlati.add(java.lang.Double.parseDouble(lati[i]))
                 dloti.add(java.lang.Double.parseDouble(loti[i]))
             }
-            var len1 = lati1.size-1
+            var len1 = lati1.size - 1
             for (i in 0..len1) {
                 dlati1.add(java.lang.Double.parseDouble(lati1[i]))
                 dloti1.add(java.lang.Double.parseDouble(loti1[i]))
             }
-        }
-        else{
-            var len = lati.size-1
+        } else {
+            var len = lati.size - 1
             for (i in 0..len) {
                 dlati.add(java.lang.Double.parseDouble(lati[i]))
                 dloti.add(java.lang.Double.parseDouble(loti[i]))
@@ -118,7 +116,7 @@ class Fragment1 : NMapFragment(), NMapView.OnMapStateChangeListener, NMapPOIdata
 
     override fun onStart() {
         super.onStart()
-        mapView?.setBuiltInZoomControls(true,null)
+        mapView?.setBuiltInZoomControls(true, null)
         mapView?.setOnMapStateChangeListener(this)
         mapController = mapView?.mapController
         mapViewerResourceProvider = NMapViewerResourceProvider(activity)
@@ -147,11 +145,11 @@ class Fragment1 : NMapFragment(), NMapView.OnMapStateChangeListener, NMapPOIdata
     }
 
     private fun moveMapCenter() {
-        if(dlati.size>0) {
-            var len = dlati.size-1
-            val currentPoint = NGeoPoint(dlati[len/2], dloti[len/2])
+        if (dlati.size > 0) {
+            var len = dlati.size - 1
+            val currentPoint = NGeoPoint(dlati[len / 2], dloti[len / 2])
 
-            if(cnum>1) {
+            if (cnum > 1) {
                 val poiData = NMapPOIdata(2, mapViewerResourceProvider)
                 poiData.addPOIitem(dlati[0], dloti[0], "", NMapPOIflagType.FROM, 0)
                 poiData.addPOIitem(dlati[len], dloti[len], "", NMapPOIflagType.TO, 0)
@@ -166,21 +164,21 @@ class Fragment1 : NMapFragment(), NMapView.OnMapStateChangeListener, NMapPOIdata
             val pathData = NMapPathData(len)
             pathData.initPathData()
 
-            for(i in 0..len) {
-                if(i==0)
+            for (i in 0..len) {
+                if (i == 0)
                     pathData.addPathPoint(dlati[i], dloti[i], NMapPathLineStyle.TYPE_SOLID)
                 else
                     pathData.addPathPoint(dlati[i], dloti[i], 0)
             }
             pathData.endPathData()
             val pathDataOverlay = mapOverlayManager!!.createPathDataOverlay(pathData)
-            pathDataOverlay.setLineColor(odds[cnum],1000)
+            pathDataOverlay.setLineColor(odds[cnum], 1000)
         }
 
-        if(dlati1.size>0){
-            var len = dlati1.size-1
-            val currentPoint = NGeoPoint(dlati1[len/2], dloti1[len/2])
-            if(cnum==1) {
+        if (dlati1.size > 0) {
+            var len = dlati1.size - 1
+            val currentPoint = NGeoPoint(dlati1[len / 2], dloti1[len / 2])
+            if (cnum == 1) {
                 val poiData = NMapPOIdata(2, mapViewerResourceProvider)
                 poiData.addPOIitem(dlati1[0], dloti1[0], "", NMapPOIflagType.FROM, 0)
                 poiData.addPOIitem(dlati1[len], dloti1[len], "", NMapPOIflagType.TO, 0)
@@ -194,15 +192,15 @@ class Fragment1 : NMapFragment(), NMapView.OnMapStateChangeListener, NMapPOIdata
             mapController!!.zoomLevel = 8
             val pathData = NMapPathData(len)
             pathData.initPathData()
-            for(i in 0..len) {
-                if(i==0)
+            for (i in 0..len) {
+                if (i == 0)
                     pathData.addPathPoint(dlati1[i], dloti1[i], NMapPathLineStyle.TYPE_SOLID)
                 else
                     pathData.addPathPoint(dlati1[i], dloti1[i], 0)
             }
             pathData.endPathData()
             val pathDataOverlay = mapOverlayManager!!.createPathDataOverlay(pathData)
-            pathDataOverlay.setLineColor(odds[cnum],1000)
+            pathDataOverlay.setLineColor(odds[cnum], 1000)
         }
     }
 
